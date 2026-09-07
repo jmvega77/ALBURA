08ddc7db40d869ccbfdf223321d4a690d6b16250
jmvega77 <jmvega77@gmail.com>
2026-09-07 13:28:24 +0200

    Conectar Personal con la pantalla principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import AttentionSection from './components/AttentionSection'
 import ApplicationsSection from './components/ApplicationsSection'
 import WelcomeSection from './components/WelcomeSection'
+import Personal from './Personal'
 
 function App() {
+  const [activeView, setActiveView] = useState<'home' | 'personal'>('home')
+
+  if (activeView === 'personal') {
+    return <Personal onBack={() => setActiveView('home')} />
+  }
+
   return (
     <div className="albura-app">
       <Header />
@@ -14,7 +22,7 @@ function App() {
 
         <AttentionSection />
 
-        <ApplicationsSection />
+        <ApplicationsSection onPersonal={() => setActiveView('personal')} />
       </main>
     </div>
   )
