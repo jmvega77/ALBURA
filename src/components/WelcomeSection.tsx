8f4b23438d1756a0f1348c9ad4a80fded78948d3
jmvega77 <jmvega77@gmail.com>
2026-09-07 12:59:59 +0200

    Separar sección de bienvenida

---
+++ src/components/WelcomeSection.tsx
@@ -0,0 +1,13 @@
+function WelcomeSection() {
+  return (
+    <section className="welcome-section">
+      <span className="welcome-label">HOY</span>
+
+      <h1>Buenos días, Chete</h1>
+
+      <p>Esto es lo que requiere tu atención hoy</p>
+    </section>
+  )
+}
+
+export default WelcomeSection
